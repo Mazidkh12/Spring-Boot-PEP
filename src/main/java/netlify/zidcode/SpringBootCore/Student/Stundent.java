04bc3ba25ae5acec81d7545ce68e174a2e6c8afa
Mazidkh12 <mazidkhanss12@gmail.com>
2026-07-13 11:02:50 +0530

    Work updated

---
+++ src/main/java/netlify/zidcode/SpringBootCore/Student/Stundent.java
@@ -0,0 +1,31 @@
+package netlify.zidcode.SpringBootCore;
+
+public class Stundent {
+    private String name;
+    private int age;
+
+    public Stundent() {
+
+    }
+
+    public Stundent(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

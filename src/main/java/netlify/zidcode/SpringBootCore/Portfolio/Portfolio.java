3bb5876785a134ab59d8079c919292c9415c785e
Mazidkh12 <mazidkhanss12@gmail.com>
2026-07-14 11:24:57 +0530

    Multiple Route create

--- src/main/java/netlify/zidcode/SpringBootCore/Portfolio/Portfolio.java
+++ src/main/java/netlify/zidcode/SpringBootCore/Portfolio/Portfolio.java
@@ -13,7 +13,46 @@ public class Portfolio {
                 <ul>
                 <li> Github link: <a>https://github.com/Mazidkh12 </a></li>
                 <li> LinkedIn link: <a>https://linkedin.com/Mazidkhan12 </a></li>
-                </ul?
+                </ul>
+                """;
+    }
+
+    @GetMapping("/skills")
+    public String mySkills() {
+        return """
+                <h1>Full Stack Developer</h1>
+                <p> I am a passionate Full Stack Developer. I am a critical thinker and problem solver </p>
+                <ul>
+                <li> Java Spring Boot </li>
+                <li> DSA </li>
+                <li> MERN </li>
+                </ul>
+                """;
+    }
+
+    @GetMapping("/edu")
+    public String edu() {
+        return """
+                <h1>Bachelor of Technology</h1>
+                <p> I am currently pursuing B. Tech in CSE at Lovely Professional University </p>
+                <ul>
+                <li> Java Spring Boot </li>
+                <li> DSA </li>
+                <li> MERN </li>
+                </ul>
+                """;
+    }
+
+    @GetMapping("/projects")
+    public String projects() {
+        return """
+                <h1>AlphaInvest Agent</h1>
+                <p> it takes a company name, does its research, and decides whether to invest or pass — with the reasoning behind its decision. </p>
+                <ul>
+                <li> Java Spring Boot </li>
+                <li> DSA </li>
+                <li> MERN </li>
+                </ul>
                 """;
     }
 }
